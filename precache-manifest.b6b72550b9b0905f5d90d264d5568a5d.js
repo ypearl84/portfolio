b52eaeb69a66f7dfd1e0e60ff71221b4d23eddea
ypearl84 <y.pearl84@gmail.com>
2019-12-09 15:06:34 -0800

    Updates

---
+++ precache-manifest.b6b72550b9b0905f5d90d264d5568a5d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d601b170326d34066f77d7e9bc601af",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4fbc8b6a24c326c901de",
+    "url": "/portfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "dd0e9f8937adcb986123",
+    "url": "/portfolio/static/js/2.06bed6cc.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.06bed6cc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4fbc8b6a24c326c901de",
+    "url": "/portfolio/static/js/main.edf00527.chunk.js"
+  },
+  {
+    "revision": "970cb49af8663eaed3c4",
+    "url": "/portfolio/static/js/runtime-main.fd1cf254.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
